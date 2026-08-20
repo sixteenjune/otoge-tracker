5a3c8cb2320696d7cdc05d92fc22a4abf437b151
sixteenjune <226630085+sixteenjune@users.noreply.github.com>
2026-08-20 11:44:36 +1000

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins: [react()], base: process.env.BASE_PATH || "/" });
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/otoge-tracker/",
+});
+
